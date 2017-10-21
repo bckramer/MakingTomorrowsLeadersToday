a47bcd3c6acd6427506e181a31c4046176d29f57
bckramer <bckramer97@gmail.com>
2017-10-20 20:15:51 -0500

    More work on Neuron - Ben

--- src/NeuralNetwork/NeuralNetwork/Neuron.java
+++ src/NeuralNetwork/NeuralNetwork/Neuron.java
@@ -1,6 +1,7 @@
 package NeuralNetwork;
 
 import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Neuron {
@@ -8,13 +9,22 @@ public class Neuron {
     //Identifier
     private String id;
     //Connections giving info to the neuron
-    protected List<Connection> inputConnection;
+    protected List<Connection> inputConnections;
     //Connections stemming out from the neuron
-    protected List<Connection> outputConnection;
+    protected List<Connection> outputConnections;
     //
     protected InputSummingFunction inputSummingFunction;
     //
     protected ActivationFunction activationFunction;
 
+    public Neuron() {
+        this.inputConnections = new ArrayList<>();
+        this.outputConnections = new ArrayList<>();
+    }
 
+    public double calculateOutput() {
+        double totalInput = inputSummingFunction.getOutput(inputConnections);
+
+        return activationFunction.getOutput(totalInput);
+    }
 }
